4b2b5e8b77e510ccc16ab5c1f5b147cee64193ae
KishanHarode <kishanharode0@gmail.com>
2025-03-16 13:28:08 +0530

    Initial commit:Add_to_cart_foodApplication

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -1,39 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FoodCard from "./FoodCard";
 import FoodData from "../data/FoodData.js";
 import toast, { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
+import { PropagateLoader } from "react-spinners";
 
 const FoodItems = () => {
   const category = useSelector((state) => state.category.category);
   const search = useSelector((state) => state.search.search);
   const handleToast = (name) => toast.success(`Added ${name} `);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+  }, [])
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
-      <div className="flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10">
-        {FoodData.filter((food) => {
-          if (category === "All") {
-            return food.name.toLowerCase().includes(search.toLowerCase());
-          } else {
-            return (
-              category === food.category &&
-              food.name.toLowerCase().includes(search.toLowerCase())
-            );
-          }
-        }).map((food) => (
-          <FoodCard
-            key={food.id}
-            id={food.id}
-            name={food.name}
-            price={food.price}
-            desc={food.desc}
-            rating={food.rating}
-            img={food.img}
-            handleToast={handleToast}
-          />
-        ))}
-      </div>
+      {
+        loading ? (
+          <div className="flex items-center justify-center mt-[45px]">
+            <PropagateLoader color="#36d7b7"/>
+          </div>
+        ) : (
+          <div className="flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10">
+            {FoodData.filter((food) => {
+              if (category === "All") {
+                return food.name.toLowerCase().includes(search.toLowerCase());
+              } else {
+                return (
+                  category === food.category &&
+                  food.name.toLowerCase().includes(search.toLowerCase())
+                );
+              }
+            }).map((food) => (
+              <FoodCard
+                key={food.id}
+                id={food.id}
+                name={food.name}
+                price={food.price}
+                desc={food.desc}
+                rating={food.rating}
+                img={food.img}
+                handleToast={handleToast}
+              />
+            ))}
+          </div>
+        )
+      }
+
     </>
   );
 };
